72eabedcbc83aefdbbc4eb5f351f77f06c31b096
sh4vyar <wolfkurd66@gmail.com>
2025-01-02 19:18:39 +0300

    fix: shadcn theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Provider } from "@/components/provider";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "title",
@@ -14,6 +15,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="white"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="black"
+        />
+      </Head>
       <body>
         <Provider
           attribute="class"
